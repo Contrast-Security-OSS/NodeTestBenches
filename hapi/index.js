a6d3f9cb3b3c15d0685c199bef949b1af3da03d2
Ehden Sinai <ehden.sinai@contrastsecurity.com>
2017-09-08 19:18:35 -0400

    basic app layout

--- hapi/index.js
+++ hapi/index.js
@@ -1,14 +1,58 @@
 'use strict';
+/* eslint-disable */
 
 const Hapi = require('hapi');
+const server = new Hapi.Server({
+	connections: {
+		routes: {
+			files: {
+				relativeTo: __dirname
+			}
+		}
+	}
+});
 
-const server = new Hapi.Server();
 server.connection({port: 3000, host: 'localhost'});
 server.start((err) => {
 	if (err) {
 		throw err;
 	}
 	console.log(`Server running at: ${server.info.uri}`); // eslint-disable-line
+});
+
+server.register(require('inert'), err => {
+	server.route({
+		method: 'GET',
+		path: '/public/app.css',
+		handler: function(request, reply) {
+			reply.file('public/app.css');
+		}
+	});
+	// method: 'GET',
+	// path: 'public/app.css',
+	// handler: function() {
+	// }
+});
+server.register(require('vision'), err => {
+	server.views({
+		engines: {
+			ejs: require('ejs')
+		},
+		relativeTo: __dirname,
+		path: 'views',
+		partialsPath: 'views/partials'
+	});
+});
+
+server.route({
+	method: 'GET',
+	path: '/',
+	handler: {
+		view: 'pages/index'
+	}
+	// handler: function(req, reply) {
+	// 	reply('');
+	// }
 });
 
 server.route({

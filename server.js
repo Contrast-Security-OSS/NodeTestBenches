28d06b25dbc9810ff69460d63c47c82368dc3af5
Ehden Sinai <ehden.sinai@contrastsecurity.com>
2017-09-11 13:41:36 -0400

    CONTRAST-17080. add basic routing/structure

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const glue = require('glue');
+const manifest = {
+	connections: [{
+		port: 3000
+	}],
+	registrations: [
+		{plugin: 'inert'},
+		{plugin: 'vision'},
+		{
+			plugin: {
+				register: 'visionary',
+				options: {
+					engines: {
+						ejs: require('ejs')
+					},
+					relativeTo: __dirname,
+					path: 'views',
+					partialsPath: 'views/partials'
+				}
+			}
+
+		},
+		{plugin: './routes/index.js'}
+	]
+};
+
+const options = {
+	relativeTo: __dirname
+};
+
+glue.compose(manifest, options, (err, server) => {
+	if (err) {
+		throw err;
+	}
+	server.start(() => {
+		console.log(`Server running at: ${server.info.uri}`); // eslint-disable-line
+	});
+});

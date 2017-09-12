17ed29294bf8413b70b267500fe03f480dd5af47
Ehden Sinai <ehden.sinai@contrastsecurity.com>
2017-09-11 23:44:58 -0400

    cleanup

--- hapi/routes/mongo-injection/index.js
+++ hapi/routes/mongo-injection/index.js
@@ -1,13 +1,13 @@
 'use strict';
+const Hoek = require('hoek');
 const pluginName = 'hapitestbench-mongoinjection';
-let db;
 
 /**
- * @param {string} type Name of the property of request to get the input from
- * @param {boolean} safe Whether or not to make the route safe
+ * @param {Object} db    - The mongo db instance to do stuff on
+ * @param {string} type  - Name of the property of request to get the input from
+ * @param {boolean} safe - Whether or not to make the route safe
  */
-function baseHandler (type, safe, request, reply) {
-	if (!db) { reply(new Error('mongo db not properly initialized')); }
+function baseHandler (db, type, safe, request, reply) {
 	const input = safe ? '' : request[type].input;
 	db.eval('db.hapitestbench.find(' + input  + ')', function(err, result) {
 		if (err) {
@@ -18,29 +18,32 @@ function baseHandler (type, safe, request, reply) {
 	});
 }
 
-function makeHandler (type, safe) {
-	return baseHandler.bind(this, type, safe);
+function makeHandler (db, type, safe) {
+	return baseHandler.bind(this, db, type, safe);
 }
 
-// curl http://localhost:3000/mongoinjection/header --header "input: hi_header"
-// curl http://localhost:3000/mongoinjection/headerSafe --header "input: hi_header"
-// curl http://localhost:3000/mongoinjection/cookie --cookie "input=hi_cookie"
-// curl http://localhost:3000/mongoinjection/cookieSafe --cookie "input=hi_cookie"
-const handlers = {
-	query:      makeHandler('query', false),
-	querySafe:  makeHandler('query', true),
-	param:      makeHandler('params', false),
-	paramSafe:  makeHandler('params', true),
-	header:     makeHandler('headers', false),
-	headerSafe: makeHandler('headers', true),
-	cookie:     makeHandler('state', false),
-	cookieSafe: makeHandler('state', true),
-	post:       makeHandler('payload', false),
-	postSafe:   makeHandler('payload', true)
-};
-
 exports.register = function mongoInjection(server, options, next) {
-	db = server.plugins['hapitestbench-mongo'].db;
+	const db = server.plugins['hapitestbench-mongo'].db;
+	if (!db) {
+		Hoek.assert(db, 'mongodb was not properly initialized');
+	}
+
+	// curl http://localhost:3000/mongoinjection/header --header "input: hi_header"
+	// curl http://localhost:3000/mongoinjection/headerSafe --header "input: hi_header"
+	// curl http://localhost:3000/mongoinjection/cookie --cookie "input=hi_cookie"
+	// curl http://localhost:3000/mongoinjection/cookieSafe --cookie "input=hi_cookie"
+	const handlers = {
+		query:      makeHandler(db, 'query', false),
+		querySafe:  makeHandler(db, 'query', true),
+		param:      makeHandler(db, 'params', false),
+		paramSafe:  makeHandler(db, 'params', true),
+		header:     makeHandler(db, 'headers', false),
+		headerSafe: makeHandler(db, 'headers', true),
+		cookie:     makeHandler(db, 'state', false),
+		cookieSafe: makeHandler(db, 'state', true),
+		post:       makeHandler(db, 'payload', false),
+		postSafe:   makeHandler(db, 'payload', true)
+	};
 
 	server.route([
 		{
@@ -50,16 +53,16 @@ exports.register = function mongoInjection(server, options, next) {
 				view: 'mongo-injection'
 			}
 		},
-		{method: 'GET',  path: '/query',             handler: handlers.query},
-		{method: 'GET',  path: '/querySafe',         handler: handlers.querySafe},
-		{method: 'GET',  path: '/param/{input}',     handler: handlers.query},
-		{method: 'GET',  path: '/paramSafe/{input}', handler: handlers.querySafe},
-		{method: 'POST', path: '/post',              handler: handlers.post},
-		{method: 'POST', path: '/postSafe',          handler: handlers.postSafe},
+		{method: 'GET',  path: '/cookie',            handler: handlers.cookie},
+		{method: 'GET',  path: '/cookieSafe',        handler: handlers.cookieSafe},
 		{method: 'GET',  path: '/header',            handler: handlers.header},
 		{method: 'GET',  path: '/headerSafe',        handler: handlers.headerSafe},
-		{method: 'GET',  path: '/cookie',            handler: handlers.cookie},
-		{method: 'GET',  path: '/cookieSafe',        handler: handlers.cookieSafe}
+		{method: 'GET',  path: '/param/{input}',     handler: handlers.query},
+		{method: 'GET',  path: '/paramSafe/{input}', handler: handlers.querySafe},
+		{method: 'GET',  path: '/query',             handler: handlers.query},
+		{method: 'GET',  path: '/querySafe',         handler: handlers.querySafe},
+		{method: 'POST', path: '/post',              handler: handlers.post},
+		{method: 'POST', path: '/postSafe',          handler: handlers.postSafe},
 	]);
 
 	next();

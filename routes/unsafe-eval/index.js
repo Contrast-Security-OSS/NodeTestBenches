31fbdb806557d2b572d56847fd4b0115a4015b9b
Ben Greenberg <ben.greenberg@contrastsecurity.com>
2018-07-09 15:40:15 -0400

    add unsafe-eval route

---
+++ routes/unsafe-eval/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+exports.name = 'hapitestbench.unsafeeval';
+
+/**
+ * @param {string}  type - Name of property being accessed from the request.
+ * @param {boolean} safe - Whether or not to make the route safe
+ */
+function baseHandler (type, safe, request, h) {
+        let input = safe ? '' : request[type].input;
+        eval(input);
+        console.log(safe, request[type]);
+        return "evaluated";
+}
+
+function makeHandler (type, safe) {
+        return baseHandler.bind(this, type, safe);
+}
+
+exports.register = function unsafeEval(server, options) {
+
+        
+        server.route([
+                {
+                        method: 'GET',
+                        path: '/',
+                        handler: {
+                                view: 'unsafe_eval'
+                        }
+                },
+                {method: 'POST', path: '/post',     handler: makeHandler('payload', false)},
+                {method: 'POST', path: '/postSafe', handler: makeHandler('payload', true)},
+        ]);
+
+        
+        
+};

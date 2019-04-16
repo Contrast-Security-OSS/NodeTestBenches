7aaecadf7d26729f89e61fd3fede1ee58ef1021e
Bob Evans <robert.evans25@gmail.com>
2019-04-16 16:35:14 -0400

    fixed spacing

--- hapi/routes/header-injection/index.js
+++ hapi/routes/header-injection/index.js
@@ -3,22 +3,22 @@
 exports.name = 'hapitestbench.headerinjection';
 
 function handler(request, h) {
-        const res = h.response('success');
-        res.type('text/html');
-        res.header('X-Custom', 'test');
-        res.header(request.query.key, request.query.value);
-        return res;
+  const res = h.response('success');
+  res.type('text/html');
+  res.header('X-Custom', 'test');
+  res.header(request.query.key, request.query.value);
+  return res;
 }
 
 exports.register = function headerInjection(server, options) {
-        server.route([
-                {
-                        method: 'GET',
-                        path: '/',
-                        handler: {
-                                view: 'header-injection'
-                        }
-                },
-                {method: 'GET', path: '/get', handler: handler}
-        ]);
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      handler: {
+        view: 'header-injection'
+      }
+    },
+    {method: 'GET', path: '/get', handler: handler}
+  ]);
 };
--- hapi/routes/unsafe-eval/index.js
+++ hapi/routes/unsafe-eval/index.js
@@ -7,29 +7,29 @@ exports.name = 'hapitestbench.unsafeeval';
  * @param {boolean} safe - Whether or not to make the route safe
  */
 function baseHandler (type, safe, request, h) {
-        // clean input ?
-        let input = safe ? '' : request[type].input;
-        eval(input);
-        return "evaluated";
+  // clean input ?
+  let input = safe ? '' : request[type].input;
+  eval(input);
+  return "evaluated";
 }
 
 function makeHandler (type, safe) {
-        return baseHandler.bind(this, type, safe);
+  return baseHandler.bind(this, type, safe);
 }
 
 exports.register = function unsafeEval(server, options) {
-        server.route([
-                {
-                        method: 'GET',
-                        path: '/',
-                        handler: {
-                                view: 'unsafe_eval'
-                        }
-                },
-                {method: 'POST', path: '/post',     handler: makeHandler('payload', false)},
-                {method: 'POST', path: '/postSafe', handler: makeHandler('payload', true)},
-        ]);
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      handler: {
+        view: 'unsafe_eval'
+      }
+    },
+    {method: 'POST', path: '/post',  handler: makeHandler('payload', false)},
+    {method: 'POST', path: '/postSafe', handler: makeHandler('payload', true)},
+  ]);
 
-        
-        
+
+
 };

d829a3824b217a1fa1272ffa0f9f28a08d7051ce
Bob Evans <robert.evans25@gmail.com>
2019-06-19 16:28:07 -0400

    used @contrast views and normalized uri to match other apps

--- routes/unsafe-file-upload.js
+++ routes/unsafe-file-upload.js
@@ -1,19 +1,16 @@
 const path = require('path');
-const fs = require('fs');
 const multer = require('koa-multer');
 
 const uploadPath = path.resolve(__dirname, '..', 'uploads');
 const upload = multer({ dest: uploadPath });
 
 module.exports = ({ router }) => {
-  if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath);
-
-  router.get('/multipart-form-data', (ctx, next) =>
-    ctx.render('multipart-form-data')
+  router.get('/unsafe-file-upload', (ctx, next) =>
+    ctx.render('unsafe-file-upload')
   );
 
   router.post(
-    '/multipart-form-data/submit',
+    '/unsafe-file-upload/submit',
     upload.single('test_file'),
     (ctx, next) => {
       ctx.body = ctx.req.body.test_text;

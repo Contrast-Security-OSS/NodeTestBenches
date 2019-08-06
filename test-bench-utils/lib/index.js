b36c2e5ddf9b1041ea5cca162d12dffc82ea4d11
Bob Evans <robert.evans25@gmail.com>
2019-08-06 14:30:16 -0400

    added pathpath traversal configs

--- test-bench-utils/lib/index.js
+++ test-bench-utils/lib/index.js
@@ -3,7 +3,8 @@
 module.exports = {
   sinks: {
     ssrf: require('./sinks/ssrf'),
-    cmd_injection: require('./sinks/cmd-injection')
+    cmd_injection: require('./sinks/cmd-injection'),
+    path_traversal: require('./sinks/path-traversal')
   },
   routes: require('./routes'),
   frameworkMapping: require('./frameworkMapping'),
--- test-bench-utils/lib/routes.js
+++ test-bench-utils/lib/routes.js
@@ -3,5 +3,10 @@ module.exports = {
     base: '/cmd-injection',
     applicableInputs: ['query'], // default
     sinks: ['exec', 'execSync', 'spawn', 'spawnSync']
+  },
+  path_traversal: {
+    base: '/path-traversal',
+    applicableInputs: ['query'], // default
+    sinks: ['readFile', 'readFileSync', 'writeFile', 'writeFileSync']
   }
 };

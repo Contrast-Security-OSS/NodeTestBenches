61a843ee931ae0d1364a1aedfd7420cddac66162
Bob Evans <robert.evans25@gmail.com>
2019-08-06 17:43:48 -0400

    added ssjs shared view and content

--- routes/ssjs.js
+++ routes/ssjs.js
@@ -1,12 +1,28 @@
-/**
- * @vulnerability: ssjs-injection
- */
-module.exports = ({ router }) => {
-  router.get('/ssjs', (ctx, next) => ctx.render('ssjs'));
+'use strict';
+const {
+  sinks: { ssjs },
+  routes: {
+    ssjs: { base: baseUri, sinks }
+  },
+  frameworkMapping: { koa },
+  utils: { buildUrls }
+} = require('@contrast/test-bench-utils');
 
-  // vulns
-  router.get('/ssjs/eval', (ctx, next) => {
-    const code = ctx.query.user_data;
-    ctx.body = eval(code || '');
+module.exports = ({ router }) => {
+  const { method, key } = koa.query;
+  const viewData = buildUrls({ sinks, key, baseUri });
+  router.get(baseUri, (ctx, next) => ctx.render('ssjs', { viewData }));
+
+  viewData.forEach(({ url, sink }) => {
+    router[method](`${url}/unsafe`, async (ctx, next) => {
+      const cmd = ctx[key].input;
+      const data = await ssjs[sink](cmd);
+      ctx.body = data.toString();
+    });
+
+    router[method](`${url}/safe`, (ctx, next) => {
+      // This rule has no untags so just returning untracked data
+      ctx.body = 'SAFE';
+    });
   });
 };

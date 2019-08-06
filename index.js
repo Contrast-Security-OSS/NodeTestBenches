acaa35ef6b98bd2ffff89f45b8b2968577d15c29
Bob Evans <robert.evans25@gmail.com>
2019-08-06 11:03:54 -0400

    removed unused code from yeoman generator, ported view engine to ejs

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 'use strict';
 
-var express = require('express');
-var kraken = require('kraken-js');
-
-
-var options, app;
+const express = require('express');
+const kraken = require('kraken-js');
 
 /*
  * Create and configure application. Also exports application instance for use by tests.
  * See https://github.com/krakenjs/kraken-js#options for additional configuration options.
  */
-options = {
-    onconfig: function (config, next) {
-        /*
-         * Add any additional config setup or overrides here. `config` is an initialized
-         * `confit` (https://github.com/krakenjs/confit/) configuration object.
-         */
-        next(null, config);
-    }
+const options = {
+  onconfig(config, next) {
+    /*
+     * Add any additional config setup or overrides here. `config` is an initialized
+     * `confit` (https://github.com/krakenjs/confit/) configuration object.
+     */
+    next(null, config);
+  }
 };
 
-app = module.exports = express();
+const app = (module.exports = express());
+app.set('view engine', 'ejs');
+app.locals.currentYear = new Date().getFullYear();
 app.use(kraken(options));
-app.on('start', function () {
-    console.log('Application ready to serve requests.');
-    console.log('Environment: %s', app.kraken.get('env:env'));
+app.on('start', function() {
+  console.log('Application ready to serve requests.');
+  console.log('Environment: %s', app.kraken.get('env:env'));
 });

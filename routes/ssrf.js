55cb92d0da16df7fde71f7538d214b0931de5f46
Bob Evans <robert.evans25@gmail.com>
2019-08-06 15:37:21 -0400

    added shared utils to ssrf

--- routes/ssrf.js
+++ routes/ssrf.js
@@ -1,53 +1,35 @@
 'use strict';
 
 const {
+  routes: {
+    ssrf: { base, sinks }
+  },
+  frameworkMapping: { koa },
   sinks: { ssrf }
 } = require('@contrast/test-bench-utils');
 
 const EXAMPLE_URL = 'http://www.example.com';
 
 module.exports = ({ router }) => {
-  router.get('/ssrf', (ctx) =>
+  const { method, key } = koa.query;
+  router.get(base, (ctx) =>
     ctx.render('ssrf', {
       requestUrl: EXAMPLE_URL
     })
   );
 
-  const libs = ['axios', 'bent', 'fetch', 'request', 'superagent'];
-
-  libs.forEach((lib) => {
-    router.get(`/ssrf/${lib}/query/unsafe`, async (ctx, next) => {
-      const url = createUnsafeUrl(ctx.query.input);
-      const data = await makeRequest(lib, url);
-
+  sinks.forEach((sink) => {
+    const lib = sink.toLowerCase();
+    router[method](`${base}/${lib}/query`, async function(ctx, next) {
+      const url = `${EXAMPLE_URL}?q=${ctx[key].input}`;
+      const data = await ssrf[`make${sink}Request`](url);
       ctx.body = data;
     });
 
-    router.post(`/ssrf/${lib}/body/unsafe`, async (ctx, next) => {
-      const url = createUnsafeUrl(ctx.request.body.input);
-      const data = await makeRequest(lib, url);
-
+    router[method](`${base}/${lib}/path`, async function(ctx, next) {
+      const url = `http://${ctx[key].input}`;
+      const data = await ssrf[`make${sink}Request`](url);
       ctx.body = data;
     });
   });
-};
-
-const createUnsafeUrl = (input) => `${EXAMPLE_URL}?q=${input}`;
-
-const createSafeUrl = (input) =>
-  `${EXAMPLE_URL}?q=${encodeURIComponent(input)}`;
-
-const makeRequest = async function makeRequest(lib, url) {
-  switch (lib) {
-    case 'axios':
-      return ssrf.makeAxiosRequest(url);
-    case 'bent':
-      return ssrf.makeBentRequest(url);
-    case 'fetch':
-      return ssrf.makeFetchRequest(url);
-    case 'request':
-      return ssrf.makeRequestRequest(url);
-    case 'superagent':
-      return ssrf.makeSuperagentRequest(url);
-  }
 };

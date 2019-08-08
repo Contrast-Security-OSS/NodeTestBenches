28c83803f49111450cc5788fe18935bee93943d1
Griffin Yourick <gryphon92@gmail.com>
2019-08-08 13:05:42 -0400

    Add SQL Injection functionality from shared test bench utils

--- routes/sql-injection/index.js
+++ routes/sql-injection/index.js
@@ -1,62 +1,49 @@
 'use strict';
 const Hoek = require('@hapi/hoek');
+const util = require('util');
+
+const {
+  sinks,
+  routes,
+  frameworkMapping,
+  utils
+} = require('@contrast/test-bench-utils');
 
 exports.name = 'hapitestbench.sqlinjection';
 
-/**
- * @param {Object} db    - The mongo db instance to do stuff on
- * @param {string} type  - Name of the property of request to get the input from
- * @param {boolean} safe - Whether or not to make the route safe
- */
-function baseHandler(db, type, safe, request, h) {
-  const input = safe ? '' : request[type].input;
-  return db.query(`SELECT "${input}" as "test";`);
-}
+exports.register = function sqlInjection(server, options) {
+  const { method, key } = frameworkMapping.hapi.query;
+  const viewData = utils.buildUrls({
+    key,
+    sinks: routes.sqli.sinks,
+    baseUri: routes.sqli.base
+  });
 
-function makeHandler(db, type, safe) {
-  return baseHandler.bind(this, db, type, safe);
-}
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => h.view('sql-injection', { viewData })
+  });
 
-exports.register = function mongoInjection(server, options) {
-  const { db } = server.plugins['hapitestbench.mysql'];
-  if (!db) {
-    return Hoek.assert(db, 'mysql connection was not properly initialized');
-  }
-
-  // curl http://localhost:3000/sqlinjection/header --header "input: hi_header"
-  // curl http://localhost:3000/sqlinjection/headerSafe --header "input: hi_header"
-  // curl http://localhost:3000/sqlinjection/cookie --cookie "input=hi_cookie"
-  // curl http://localhost:3000/sqlinjection/cookieSafe --cookie "input=hi_cookie"
-  const handlers = {
-    query: makeHandler(db, 'query', false),
-    querySafe: makeHandler(db, 'query', true),
-    param: makeHandler(db, 'params', false),
-    paramSafe: makeHandler(db, 'params', true),
-    header: makeHandler(db, 'headers', false),
-    headerSafe: makeHandler(db, 'headers', true),
-    cookie: makeHandler(db, 'state', false),
-    cookieSafe: makeHandler(db, 'state', true),
-    post: makeHandler(db, 'payload', false),
-    postSafe: makeHandler(db, 'payload', true)
-  };
-
-  server.route([
-    {
-      method: 'GET',
-      path: '/',
-      handler: {
-        view: 'sql-injection'
+  viewData.forEach(({ uri, sink }) => {
+    server.route([
+      {
+        path: `${uri}/safe`,
+        method: [method],
+        handler: async (request, h) => {
+          const result = await sinks.sqli[sink]('clown');
+          return util.inspect(result);
+        }
+      },
+      {
+        path: `${uri}/unsafe`,
+        method: [method],
+        handler: async (request, h) => {
+          const value = Hoek.reach(request, `${key}.input`) || '';
+          const result = await sinks.sqli[sink](value);
+          return util.inspect(result);
+        }
       }
-    },
-    { method: 'GET', path: '/cookie', handler: handlers.cookie },
-    { method: 'GET', path: '/cookieSafe', handler: handlers.cookieSafe },
-    { method: 'GET', path: '/header', handler: handlers.header },
-    { method: 'GET', path: '/headerSafe', handler: handlers.headerSafe },
-    { method: 'GET', path: '/param/{input}', handler: handlers.param },
-    { method: 'GET', path: '/paramSafe/{input}', handler: handlers.paramSafe },
-    { method: 'GET', path: '/query', handler: handlers.query },
-    { method: 'GET', path: '/querySafe', handler: handlers.querySafe },
-    { method: 'POST', path: '/post', handler: handlers.post },
-    { method: 'POST', path: '/postSafe', handler: handlers.postSafe }
-  ]);
+    ]);
+  });
 };

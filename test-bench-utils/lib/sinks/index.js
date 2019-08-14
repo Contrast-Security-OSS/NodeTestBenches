58de8cfdf915a38e48a467546beb75ace95a758f
Griffin Yourick <gryphon92@gmail.com>
2019-08-14 17:33:01 -0400

    Oops--export all sinks

--- test-bench-utils/lib/sinks/index.js
+++ test-bench-utils/lib/sinks/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 module.exports = {
+  nosqlInjection: require('./nosqlInjection'),
+  sqlInjection: require('./sqlInjection'),
   cmdInjection: require('./cmdInjection'),
   pathTraversal: require('./pathTraversal'),
-  sqlInjection: require('./sqlInjection'),
   ssjs: require('./ssjs'),
   ssrf: require('./ssrf'),
+  unsafeFileUpload: require('./unsafeFileUpload'),
+  unvalidatedRedirect: require('./unvalidatedRedirect'),
   xss: require('./xss'),
   xxe: require('./xxe')
 };

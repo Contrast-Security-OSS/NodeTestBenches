01cd0e3123de64e6c025b206e8c52999a6be26d5
Griffin Yourick <gryphon92@gmail.com>
2019-08-21 13:13:23 -0400

    Update pathTraversal model to provide grouped sink data

--- kraken/models/pathTraversal.js
+++ kraken/models/pathTraversal.js
@@ -2,10 +2,12 @@ const { utils } = require('@contrast/test-bench-utils');
 
 module.exports = function PathTraversalModel() {
   const sinkData = utils.getSinkData('pathTraversal', 'kraken');
+  const groupedSinkData = utils.groupSinkData(sinkData);
   const routeMeta = utils.getRouteMeta('pathTraversal');
 
   return {
     ...routeMeta,
-    sinkData
+    sinkData,
+    groupedSinkData
   };
 };

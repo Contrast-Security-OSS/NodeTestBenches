59db0e399cd9993e03a9ba8194dc0ad9fee043cb
Bob Evans <robert.evans25@gmail.com>
2020-02-06 11:51:20 -0500

    adding xpath.select sink

---
+++ test-bench-utils/lib/content/xpathInjection.js
@@ -0,0 +1,17 @@
+module.exports.xml = `
+<?xml version="1.0"?>
+<users>
+  <user>
+    <username>admin</username>
+    <password>admin</password>
+  </user>
+  <user>
+    <username>user1</username>
+    <password>123456</password>
+  </user>
+  <user>
+    <username>tony</username>
+    <password>ynot</password>
+  </user>
+</users>
+`;
---
+++ test-bench-utils/lib/sinks/xpathInjection.js
@@ -0,0 +1,20 @@
+'use strict';
+const xpath = require('xpath');
+const { DOMParser } = require('xmldom');
+const { xml } = require('../content/xpathInjection');
+const doc = new DOMParser().parseFromString(xml);
+
+module.exports['xpath.select'] = async function select(
+  input,
+  { safe = false, noop = false } = {}
+) {
+  if (noop) return 'NOOP';
+
+  const path = safe ? encodeURIComponent(input) : input;
+
+  return new Promise((resolve) => {
+    const searchString = `//user[username/text()='${path}']`;
+    const user = xpath.select(searchString, doc).toString();
+    resolve(user);
+  });
+};

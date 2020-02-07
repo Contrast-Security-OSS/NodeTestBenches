673574b88f5627232c9fe13e2ce9fa5884a6157d
Griffin Yourick <gryphon92@gmail.com>
2020-02-07 16:03:21 -0500

    Add unsafeFileUpload controller

--- server/boot/unsafeFileUpload.js
+++ server/boot/unsafeFileUpload.js
@@ -1,5 +1,7 @@
 // TODO: NYI
 
+const { get } = require('lodash');
+const uuid = require('uuid/v4');
 const { utils } = require('@contrast/test-bench-utils');
 
 const sinkData = utils.getSinkData('unsafeFileUpload', 'express');
@@ -7,11 +9,24 @@ const routeMeta = utils.getRouteMeta('unsafeFileUpload');
 
 module.exports = function(server) {
   const router = server.loopback.Router();
+  const model = server.loopback.getModel('unsafeFileUpload');
 
   router.get('/', function(req, res, next) {
     res.render('pages/unsafeFileUpload', {
       ...routeMeta,
       sinkData
+    });
+  });
+
+  sinkData.forEach(({ method, uri, sink, key }) => {
+    router[method](uri, async (req, res) => {
+      const input = get(req, [key, 'input']);
+      const result = await sink(input);
+
+      const container = await model.createContainer({ name: uuid() });
+      const uploadResult = await model.upload(container.name, req, res);
+
+      res.send({ result, uploadResult });
     });
   });
 

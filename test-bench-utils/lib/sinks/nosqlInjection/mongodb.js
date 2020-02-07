2f4601eecff26598098a4c4cce76818908aefa00
ao10 <ati.ok@contrastsecurity.com>
2020-02-07 13:59:53 -0500

    Stub internal mongo functions

--- test-bench-utils/lib/sinks/nosqlInjection/mongodb.js
+++ test-bench-utils/lib/sinks/nosqlInjection/mongodb.js
@@ -1,15 +1,20 @@
 'use strict';
 
-const { MongoClient } = require('mongodb');
-//const mock = require('mongodb-mock-server');
-// const server = mock.createServer();
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
-
 const { EventEmitter } = require('events');
 const { Db, Collection } = require('mongodb');
 
 const origEval = Db.prototype.eval;
 Db.prototype.eval = async function overloadedEval(code, params, opts) {
+  this.s.topology.s = { promiseLibrary: this.s.promiseLibrary };
+  this.s.topology.command = function() {
+    return false;
+  };
+  this.s.topology.hasSessionSupport = function() {
+    return false;
+  };
+  this.s.topology.isDestroyed = function() {
+    return false;
+  };
   // run the original eval asynchronously, ignoring any errors it will throw since we aren't connected.
   try {
     origEval.call(this, code, params, opts);
@@ -20,22 +25,8 @@ Db.prototype.eval = async function overloadedEval(code, params, opts) {
   return { code };
 };
 
-const origRename = Collection.prototype.rename;
-Collection.prototype.rename = async function overloadedRename(name, options, callback) {
-  this.s.topology.s = { promiseLibrary: this.s.promiseLibrary };
-  this.s.topology.command = function() { return false; };
-  this.s.topology.hasSessionSupport = function() { return false; }
-  try {
-    origRename.call(this, name, options, callback);
-  } catch (err) {
-    console.log(err);
-  }
-  return { name };
-};
-
 const topology = new EventEmitter();
 const db = new Db('testbench', topology, {});
-// Call Collection constructor
 const collection = new Collection(
   db,
   topology,
@@ -44,6 +35,27 @@ const collection = new Collection(
   null,
   {}
 );
+const origRename = Collection.prototype.rename;
+Collection.prototype.rename = async function overloadedRename(
+  name,
+  options,
+  callback
+) {
+  this.s.topology.s = { promiseLibrary: this.s.promiseLibrary };
+  this.s.topology.command = function() {
+    return false;
+  };
+  this.s.topology.hasSessionSupport = function() {
+    return false;
+  };
+  // run the original rename asynchronously, ignoring any errors it will throw since we aren't connected.
+  try {
+    origRename.call(this, name, options, callback);
+  } catch (err) {
+    console.log(err);
+  }
+  return { name };
+};
 
 /**
  * @param {string} input user input string
@@ -57,15 +69,9 @@ module.exports['mongodb.Db.prototype.eval'] = async function _eval(
 ) {
   if (noop) return 'NOOP';
 
-  // const client = await MongoClient.connect(url);
-  debugger;
-
   const fn = safe ? 'function() {}' : input;
   const result = await db.eval(fn);
-  // const result = await client
-  //   .db('testbench')
-  //   .eval(fn)
-  //   .catch(() => ({ code: fn }));
+
   return `<pre>${JSON.stringify(result, null, 2)}</pre>`;
 };
 
@@ -79,17 +85,10 @@ module.exports['mongodb.Collection.prototype.rename'] = async function rename(
   input,
   { safe = false, noop = false } = {}
 ) {
-  const newName = safe ? 'newName' : input;
-  // const result = await collection.rename(newName);
   if (noop) return 'NOOP';
-  const result = await collection.rename(newName);
+
+  const newName = safe ? 'newName' : input;
+  const result = await collection.rename(newName).catch((err) => {});
+
   return `<pre>${JSON.stringify(result, null, 2)}</pre>`;
 };
-
-// const client = await MongoClient.connect(url);
-// const collection = await client.db('testbench').createCollection('new');
-
-// const newName = safe ? 'newName' : input;
-// const result = await collection
-//   .rename(newName)
-//   .catch(() => ({ name: newName }));

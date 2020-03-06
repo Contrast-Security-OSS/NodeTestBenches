cfe0d5d78ea39fdc3c01e9e17f048b3a1b2ae679
Bob Evans <robert.evans25@gmail.com>
2020-03-06 09:54:48 -0500

    added semantic rules to master

---
+++ routes/cmdInjectionSemanticDangerousPaths/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const controllerFactory = require('../../utils/controllerFactory');
+
+exports.name = 'hapitestbench.cmdinjectionsemanticdangerouspaths';
+exports.register = controllerFactory('cmdInjectionSemanticDangerousPaths');

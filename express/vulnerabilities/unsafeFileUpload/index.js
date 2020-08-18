cfa42cf8cc38a55e3400cd62490bbc46f957d189
Bob Evans <robert.evans25@gmail.com>
2020-08-18 13:57:42 -0400

    fixed the ufu sinks the right way

--- express/vulnerabilities/unsafeFileUpload/index.js
+++ express/vulnerabilities/unsafeFileUpload/index.js
@@ -20,10 +20,10 @@ router.get('/', function(req, res) {
   });
 });
 
-sinkData.forEach(({ method, uri, sink, key }) => {
+sinkData.forEach(({ method, params, uri, sink, key }) => {
   router[method](uri, upload.single('file'), async (req, res) => {
-    const { input } = get(req, key);
-    const result = await sink(input); // doesn't really do anything
+    const inputs = utils.getInput(req, key, params);
+    const result = await sink(inputs); // doesn't really do anything
     res.send(result);
   });
 });

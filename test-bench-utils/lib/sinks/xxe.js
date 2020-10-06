fa972c0aa2ed18cf1da9b59f13f1a87ce82818a0
jcolekaplan <jacob.kaplan@contrastsecurity.com>
2020-10-06 11:20:38 -0400

    Refactor

--- test-bench-utils/lib/sinks/xxe.js
+++ test-bench-utils/lib/sinks/xxe.js
@@ -2,9 +2,7 @@
 let libxmljs;
 try {
   libxmljs = require('libxmljs');
-} catch (err) {
-  libxmljs = null;
-}
+} catch {}
 const libxmljs2 = require('libxmljs2');
 
 const pre = (str) => `<pre>${str}</pre>`;
@@ -16,19 +14,17 @@ const pre = (str) => `<pre>${str}</pre>`;
  * @param {boolean=} opts.safe are we calling the sink safely?
  * @param {boolean=} opts.noop are we calling the sink as a noop?
  */
-
 module.exports['libxmljs.parseXmlString'] = async function parseXmlString(
   { input },
   { safe = false, noop = false } = {}
 ) {
   if (noop) return 'NOOP';
 
-  let result;
-  if (libxmljs) {
-    result = libxmljs.parseXmlString(input, { noent: !safe });
-  } else {
-    result = libxmljs2.parseXmlString(input, { noent: !safe });
-  }
+  // Documented failure for Express on Node-14 alpine 
+  // See NODE-1062: https://contrast.atlassian.net/browse/NODE-1062
+  if (!libxmljs) return '';
+  
+  const result = libxmljs.parseXmlString(input, { noent: !safe });
   return pre(result);
 };
 

b032de35ad45cdb39d47f4d7552b7af43f27ccc2
Bob Evans <robert.evans25@gmail.com>
2020-10-20 12:03:44 -0400

    simplified the containers

--- loopback/entry.sh
+++ loopback/entry.sh
@@ -1,50 +1,20 @@
 #! /bin/bash -e
 # `CONFIG` - name of configuration file in s3
 #
-# 4 modes:
-# 1: agent from artifactory
+# 2 modes:
+# 1: agent from mounted volume
+#  - `AGENT` - boolean, will mount a dir to `/opt/contrast` to copy agent
 #  - `CONFIG` - which config from s3://node-agent-configs to use
-#  - `AGENT_VERSION` - e.g. 2.11.0
-#  - `AGENT_HASH` e.g. 20200220-2232.9aa9d178
-# 2: agent from s3 bucket:
-#  - `CONFIG` - which config from s3://node-agent-configs to use
-#  - `AGENT` - name of .tgz in s3://node-agents
-# 3: agent from mounted volume
-#  - `AGENT_LOCAL` - boolean, will mount a dir to `/opt/contrast` to copy agent
-# 4: agent-less
+# 2: agent-less
 
 if [[ -n "$AGENT" ]];
 then
-  echo "Downloading Node Agent: $AGENT from s3://node-agents"
-  aws s3 cp "s3://node-agents/$AGENT" node-agent.tgz
-elif [[ -n "$AGENT_VERSION" && -n "$AGENT_HASH" ]];
-then
-  AGENT_ARTIFACT_URL=https://contrastsecurity.jfrog.io/contrastsecurity/node-agent-staging/$AGENT_VERSION/$AGENT_HASH/node_contrast-$AGENT_VERSION.tgz
-  echo "Downloading Node Agent: $AGENT_ARTIFACT_URL"
-  responseCode=$(curl -L --silent \
-    --output node-agent.tgz \
-    -H "Authorization: Bearer $CONTRAST_ARTIFACTORY_ACCESS_TOKEN" \
-    -X GET "$AGENT_ARTIFACT_URL" \
-    --write-out "%{http_code}" )
-
-  if [[ ${responseCode} -ne 200 ]];
-  then
-    echo "Unable to download node agent - response code: $responseCode"
-    exit 1
-  fi
-elif [[ -n "$AGENT_LOCAL" ]]
-then
   cp /opt/contrast/node_contrast*.tgz ./node-agent.tgz
-else
-  echo "Running app in agent-less mode"
-  AGENT_LESS=true
-fi
-
-if [[ -n "$AGENT_LESS" ]];
-then
-  HOST=0.0.0.0 node server/server.js
-else
   aws s3 cp "s3://node-agent-configs/$CONFIG" contrast_security.yaml
   npm install node-agent.tgz
   HOST=0.0.0.0 DEBUG="contrast:*" node -r node_contrast/bootstrap server/server.js
+else
+  echo "Running app in agent-less mode"
+  HOST=0.0.0.0 node server/server.js
 fi
+

509434787d18aaf53f21088f27e4985b2cff6ab1
Bob Evans <robert.evans25@gmail.com>
2020-10-20 16:10:07 -0400

    removed the agent env var

--- loopback/entry.sh
+++ loopback/entry.sh
@@ -1,15 +1,12 @@
 #! /bin/bash -e
-# `CONFIG` - name of configuration file in s3
-#
 # 2 modes:
 # 1: agent from mounted volume
-#  - `AGENT` - boolean, will mount a dir to `/opt/contrast` to copy agent
 #  - `CONFIG` - which config from s3://node-agent-configs to use
 # 2: agent-less
 
-if [[ -n "$AGENT" ]];
+if [[ -f "/opt/contrast/node-agent.tgz" ]];
 then
-  cp /opt/contrast/node_contrast*.tgz ./node-agent.tgz
+  cp /opt/contrast/node-agent.tgz ./node-agent.tgz
   aws s3 cp "s3://node-agent-configs/$CONFIG" contrast_security.yaml
   npm install node-agent.tgz
   HOST=0.0.0.0 DEBUG="contrast:*" node -r node_contrast/bootstrap server/server.js

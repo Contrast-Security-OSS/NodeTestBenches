d925bd1af9d849e05803422b19a3c3ceda23b8a7
jcolekaplan <jacob.kaplan@contrastsecurity.com>
2020-10-22 16:15:24 -0400

    Undo

--- loopback4/src/index.ts
+++ loopback4/src/index.ts
@@ -1,17 +1,13 @@
-import {ApplicationConfig, Loopback4Application} from './application';
-
+import {ApplicationConfig} from '@loopback/core';
+import {ExpressServer} from './server';
 export * from './application';
+export {ApplicationConfig, ExpressServer};
 
 export async function main(options: ApplicationConfig = {}) {
-  const app = new Loopback4Application(options);
-  await app.boot();
-  await app.start();
-
-  const url = app.restServer.url;
-  console.log(`Server is running at ${url}`);
-  console.log(`Try ${url}/ping`);
-
-  return app;
+  const server = new ExpressServer(options);
+  await server.boot();
+  await server.start();
+  console.log(`Server is running at ${server.url}`);
 }
 
 if (require.main === module) {
@@ -30,6 +26,7 @@ if (require.main === module) {
         // useful when used with OpenAPI-to-GraphQL to locate your application
         setServersFromRequest: true,
       },
+      listenOnStart: false
     },
   };
   main(config).catch(err => {

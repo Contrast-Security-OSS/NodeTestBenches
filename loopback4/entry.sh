a12276c0e1c97e55003768f975175f8742a68637
jcolekaplan <jacob.kaplan@contrastsecurity.com>
2020-10-28 10:33:07 -0400

    Deleted unused files. Updated workflow, gitignore, Dockerfile. Added entry.sh

---
+++ loopback4/entry.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -e
+# 2 modes:
+# 1: agent from mounted volume
+#  - `CONFIG` - which config from s3://node-agent-configs to use
+# 2: agent-less
+
+if [[ -f "/opt/contrast/node-agent.tgz" ]];
+then
+  pushd /opt/contrast && tar xzf ./node-agent.tgz && popd
+  aws s3 cp "s3://node-agent-configs/$CONFIG" contrast_security.yaml
+  HOST=0.0.0.0 DEBUG="contrast:*" node -r /opt/contrast/package/bootstrap lb3app/server/server.js
+else
+  echo "Running app in agent-less mode"
+  HOST=0.0.0.0 node lb3app/server/server.js
+fi
+

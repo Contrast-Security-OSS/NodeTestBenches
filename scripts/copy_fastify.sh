46f192618e1bdd7cadb5f3ae8718fbc485b00564
jcolekaplan <jacob.kaplan@contrastsecurity.com>
2021-01-13 16:21:36 -0500

    Add fastify code copying to existing copy script. Deleted temp test workflow. Add fastify3 to build workflow

--- scripts/copy_fastify.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash -e
-folders=("public" "routes" "uploads" "utils" "view");
-cp ../fastify/server.js .
-for folder in "${folders[@]}"; do
-   cp -R -n ../fastify/$folder . 
-done
--- scripts/copy_scripts.sh
+++ scripts/copy_scripts.sh
@@ -5,3 +5,12 @@ if [[ ! -f "Dockerfile" ]];
 then
     cp ../scripts/Dockerfile .
 fi
+
+folders=("public" "routes" "uploads" "utils" "view");
+if [[ ${PWD##*/} == "fastify3" ]];
+then
+    cp ../fastify/server.js .
+    for folder in "${folders[@]}"; do
+    cp -R -n ../fastify/$folder . 
+    done
+fi

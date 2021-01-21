16090b4f41546d45a047a7670a8322ede9f5fb55
Matthew Henry <matthew.henry@contrastsecurity.com>
2021-01-21 12:41:09 -0500

    NODE-1254: do not copy config from s3

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,15 +1,13 @@
 #! /bin/bash -e
-# 2 modes:
-# 1: agent from mounted volume
-#  - `CONFIG` - which config from s3://node-agent-configs to use
-# 2: agent-less
 
 if [[ -f "/opt/contrast/node-agent.tgz" ]];
 then
+  # agent from mounted volume
   pushd /opt/contrast && tar xzf ./node-agent.tgz && popd
-  aws s3 cp "s3://node-agent-configs/$CONFIG" contrast_security.yaml
-  HOST=0.0.0.0 DEBUG="contrast:*" node -r /opt/contrast/package/bootstrap .
+  # agent configuration is done via env vars or mounted contrast_security.yaml
+  HOST=0.0.0.0 node -r /opt/contrast/package/bootstrap .
 else
+  # 2: agent-less
   echo "Running app in agent-less mode"
   HOST=0.0.0.0 node .
 fi

a6728e7ebf1efdb2dd9684e541392eaec23ef656
Yulia Tenincheva <yulia.tenincheva@contrastsecurity.com>
2021-04-02 19:32:56 +0300

    Fixed naming of kraken module for xssRenderedTemplates

--- kraken/models/xssRenderedTemplates.js
+++ kraken/models/xssRenderedTemplates.js
@@ -1,6 +1,6 @@
 const { utils } = require('@contrast/test-bench-utils');
 
-module.exports = function CrossSiteScriptingModel() {
+module.exports = function xssRenderedTemplatesModel() {
   const sinkData = utils.getSinkData('xssRenderedTemplates', 'kraken');
   const groupedSinkData = utils.groupSinkData(sinkData);
   const routeMeta = utils.getRouteMeta('xssRenderedTemplates');

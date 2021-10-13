592fc39684b7a8a3cbb81f6ae4ee77ec9afadb14
Yulia Tenincheva <yulia.tenincheva@contrastsecurity.com>
2021-10-13 16:38:29 +0300

    Added scripts to install PostgreSQL 10 in images

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,20 +1,23 @@
 #! /bin/bash -e
 
+service postgresql start
+pg_lsclusters
+
 if [[ -f "/opt/contrast/node-agent.tgz" ]];
 then
   npm config set offline
-    
+
   # agent from mounted volume
   pushd /opt/contrast && tar xzf ./node-agent.tgz && popd
   node --version
 
   # precompile code using the standalone rewriter
   if [ "$PRECOMPILE" = true ] ; then
-    echo "Run npm install"  
-    npm install /opt/contrast/node-agent.tgz --verbose  
+    echo "Run npm install"
+    npm install /opt/contrast/node-agent.tgz --verbose
 
     echo "Run npx contrast-transpile"
-    DEBUG=contrast:* npx contrast-transpile server.js
+    DEBUG="contrast:*" npx contrast-transpile server.js
   fi
 
   # agent configuration from mounted volume or env vars

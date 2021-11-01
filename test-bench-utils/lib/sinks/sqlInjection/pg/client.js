763aab11e973eabb168a7407907984145980d0d4
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2021-11-01 20:48:00 +0200

    Bump pg and make connection to real DB

--- test-bench-utils/lib/sinks/sqlInjection/pg/client.js
+++ test-bench-utils/lib/sinks/sqlInjection/pg/client.js
@@ -1,26 +1,13 @@
 'use strict';
 
 const pg = require('pg');
-const hooker = require('hooker');
-const escape = require('escape-html');
 
 const client = new pg.Client({
-  connectionString: 'localhost'
-});
-
-// Prevent the query method from requiring a real database connection.
-hooker.hook(client, 'query', {
-  post(result, config, values) {
-    // catch the original rejection
-    result.catch((err) => console.log(err));
-    // return a new promise that just resolves
-    return hooker.override(
-      new Promise((resolve, reject) => {
-        // escape it so it isn't reported as an XSS
-        resolve(escape(JSON.stringify(config)));
-      })
-    );
-  }
+  user: process.env.USER || 'contrast',
+  host: 'localhost',
+  database: process.env.USER || 'contrast',
+  password: null,
+  port: 5432
 });
 
 module.exports = client;
--- test-bench-utils/lib/sinks/sqlInjection/pg/object.js
+++ test-bench-utils/lib/sinks/sqlInjection/pg/object.js
@@ -2,6 +2,18 @@
 
 const client = require('./client');
 
+const initDb = async () => {
+  if (!client._connected) {
+    await client.connect();
+  }
+  client.query('DROP TABLE IF EXISTS students');
+  client.query(
+    'CREATE TABLE IF NOT EXISTS students(firstName text, lastName text)'
+  );
+
+  return client;
+};
+
 /**
  * @param {Object} params
  * @param {string} params.input user input string
@@ -13,6 +25,8 @@ module.exports = async function pgQuery(
   { input },
   { safe = false, noop = false } = {}
 ) {
+  const client = await initDb();
+
   if (noop) {
     return 'NOOP';
   }

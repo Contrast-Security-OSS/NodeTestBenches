7c7b9b03a8aced66be24a05359875d5caeb1ff54
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2021-12-17 17:13:43 +0200

    Add NoSQL Expansion route for Sails

---
+++ sails/vulnerabilities/nosqlExpansion.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const controllerFactory = require('../utils/controllerFactory');
+
+/**
+ * @vulnerability: nosql-expansion
+ * @vulnerability: nosql-expansion-mongo
+ */
+ module.exports = function(app, locals) {
+  return controllerFactory('nosqlExpansion', app, {locals});
+}

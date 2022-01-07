7cc2aa4851697e8b617c3e533acd2532ce0177e8
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2022-01-07 16:17:07 +0200

    Bump package-lock

--- test-bench-utils/types/sinks/nosqlExpansion.d.ts
+++ test-bench-utils/types/sinks/nosqlExpansion.d.ts
@@ -3,16 +3,16 @@ export function _mongodb_Db_prototype_find__Expansion_Query_({ input }: {
 }, { safe, noop }?: {
     safe?: boolean;
     noop?: boolean;
-}): Promise<any>;
+}): Promise<string>;
 export function _mongodb_Db_prototype_delete__Expansion_Query_({ input }: {
     input: any;
 }, { safe, noop }?: {
     safe?: boolean;
     noop?: boolean;
-}): Promise<any>;
+}): Promise<string>;
 export function _mongodb_Db_prototype_update__Expansion_Query_({ input }: {
     input: any;
 }, { safe, noop }?: {
     safe?: boolean;
     noop?: boolean;
-}): Promise<any>;
+}): Promise<string>;

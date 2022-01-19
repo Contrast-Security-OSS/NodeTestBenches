88e5dde44e762cd4b3df54a0df72025d98b72558
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2022-01-19 15:35:51 +0200

    Fix prettier error

--- loopback4/src/vulnerabilities/nosqlExpansion.ts
+++ loopback4/src/vulnerabilities/nosqlExpansion.ts
@@ -2,20 +2,20 @@ import {controllerFactory} from './controllerFactory';
 import {decode} from 'html-entities';
 
 export const nosqlExpansionControllers = controllerFactory('nosqlExpansion', {
-    respond(result, req, res) {
-      return decode(result);
-    },
-    response: {
-      description: `nosqlExpansion return value`,
-      content: {
-        'application/json': {
-          schema: {
-            type: 'object',
-            properties: {
-              input: {type: 'string'},
-            },
+  respond(result, req, res) {
+    return decode(result);
+  },
+  response: {
+    description: `nosqlExpansion return value`,
+    content: {
+      'application/json': {
+        schema: {
+          type: 'object',
+          properties: {
+            input: {type: 'string'},
           },
         },
       },
     },
-  });
+  },
+});

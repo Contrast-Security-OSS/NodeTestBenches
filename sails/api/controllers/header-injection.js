ee0e7c68cfd5ac20fd41b8c5483165c2b6139c76
Yulia Tenincheva <yulia.tenincheva@contrastsecurity.com>
2022-01-30 22:16:51 +0200

    Fixed CSP endpoints (#230)

--- sails/api/controllers/header-injection.js
+++ sails/api/controllers/header-injection.js
@@ -1,9 +1,9 @@
 module.exports = function(req, res) {
   const path = req.query.user_path;
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.setHeader('Pragma', 'no-cache');
-  res.setHeader('Expires', 0);
+  res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', 0);
+  res.set('INJECTED', path);
 
-  res.setHeader('INJECTED', path);
   res.send(`Injected header  ${path}`);
 };
--- sails/vulnerabilities/cspHeaderMissing.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function(app, locals) {
-  return require('../utils/controllerFactory')('cspHeaderMissing', app, { locals });
-}

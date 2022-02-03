f1798d674eb505ee01166c14e51ba2d350b0ed0d
Yulia Tenincheva <yulia.tenincheva@contrastsecurity.com>
2022-02-03 20:58:04 +0200

    Changed the condition for pgsql safe/unsafe routes if there is no db connection

--- test-bench-utils/lib/sinks/nosqlInjection/rethinkdb/r-js.js
+++ test-bench-utils/lib/sinks/nosqlInjection/rethinkdb/r-js.js
@@ -16,67 +16,62 @@ module.exports = async function reDbQuery(
 ) {
   if (noop) return 'NOOP';
 
-  try {
-    return await new Promise((resolve, reject) => {
-      dbInit
-        .then(() => {
-          r.connect(connectionParams)
-            .then((conn) => {
-              if (safe) {
-                const safeInput = Joi.number()
-                  .integer()
-                  .min(0)
-                  .max(100);
-                const minAge = safeInput.validate(input);
-                if (minAge.error) {
-                  resolve(minAge.error);
-                }
-                r.table('users')
-                  .filter(function(user) {
-                    return user('age').gt(minAge.value);
-                  })
-                  .run(conn)
-                  .then((response) => {
-                    response.toArray().then((formattedResponse) => {
-                      const result = formattedResponse.map(
-                        ({ name, age, addresses }) => ({
-                          name,
-                          age,
-                          addresses
-                        })
-                      );
-                      resolve(result);
-                    });
-                  })
-                  .catch((err) => reject(err));
-              } else {
-                const jsString = dotJsFnString(input);
-                r.table('users')
-                  .filter(r.js(jsString, { timeout: 3 }))
-                  .run(conn)
-                  .then((response) => {
-                    response.toArray().then((formattedResponse) => {
-                      const result = formattedResponse.map(
-                        ({ name, age, addresses }) => ({
-                          name,
-                          age,
-                          addresses
-                        })
-                      );
-                      resolve(JSON.stringify(result));
-                    });
-                  })
-                  .catch((err) => reject(err));
+  return new Promise((resolve, reject) => {
+    dbInit
+      .then(() => {
+        r.connect(connectionParams)
+          .then((conn) => {
+            if (safe) {
+              const safeInput = Joi.number()
+                .integer()
+                .min(0)
+                .max(100);
+              const minAge = safeInput.validate(input);
+              if (minAge.error) {
+                resolve(minAge.error);
               }
-            })
-            .catch((err) => reject(err));
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
+              r.table('users')
+                .filter(function(user) {
+                  return user('age').gt(minAge.value);
+                })
+                .run(conn)
+                .then((response) => {
+                  response.toArray().then((formattedResponse) => {
+                    const result = formattedResponse.map(
+                      ({ name, age, addresses }) => ({
+                        name,
+                        age,
+                        addresses
+                      })
+                    );
+                    resolve(result);
+                  });
+                })
+                .catch((err) => reject(err));
+            } else {
+              const jsString = dotJsFnString(input);
+              r.table('users')
+                .filter(r.js(jsString, { timeout: 3 }))
+                .run(conn)
+                .then((response) => {
+                  response.toArray().then((formattedResponse) => {
+                    const result = formattedResponse.map(
+                      ({ name, age, addresses }) => ({
+                        name,
+                        age,
+                        addresses
+                      })
+                    );
+                    resolve(JSON.stringify(result));
+                  });
+                })
+                .catch((err) => reject(err));
+            }
+          })
+          .catch((err) => reject(err));
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };

d97c0dbbe11ff7f6daf81b9f341c0837ca186f52
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2022-10-06 21:54:00 +0300

    Bump package-lock

--- test-bench-utils/types/sinks/pathTraversalSemanticFileSecurityBypass.d.ts
+++ test-bench-utils/types/sinks/pathTraversalSemanticFileSecurityBypass.d.ts
@@ -1,3 +1,9 @@
+export function _fs_readFile({ input }: {
+    input: string;
+}, { safe, noop }?: {
+    safe?: boolean;
+    noop?: boolean;
+}): Promise<any>;
 export function _fs_readFileSync({ input }: {
     input: string;
 }, { safe, noop }?: {
@@ -8,3 +14,7 @@ export function _child_process_exec({ input }: any, { safe, noop }?: {
     safe?: boolean;
     noop?: boolean;
 }): Promise<any>;
+export function _child_process_execSync({ input }: any, { safe, noop }?: {
+    safe?: boolean;
+    noop?: boolean;
+}): Promise<string>;

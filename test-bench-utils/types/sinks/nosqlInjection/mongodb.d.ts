f02b0e6e7b49f8c2111e895770f03e872aa34999
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2022-10-20 15:56:21 +0300

    Bump package-lock

--- test-bench-utils/types/sinks/nosqlInjection/mongodb.d.ts
+++ test-bench-utils/types/sinks/nosqlInjection/mongodb.d.ts
@@ -1,9 +1,3 @@
-export function _mongodb_Db_prototype_eval({ input }: {
-    input: string;
-}, { safe, noop }?: {
-    safe?: boolean;
-    noop?: boolean;
-}): Promise<string>;
 export function _mongodb_Collection_prototype_rename({ input }: {
     input: string;
 }, { safe, noop }?: {

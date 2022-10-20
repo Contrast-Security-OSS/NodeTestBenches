171bb1e4d08b2daa630c96e11c4d36c21fde6c5a
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2022-10-20 15:50:23 +0300

    Fix lint error

--- loopback4/src/vulnerabilities/nosqlInjection.ts
+++ loopback4/src/vulnerabilities/nosqlInjection.ts
@@ -1,10 +1,7 @@
 import {utils} from '@contrast/test-bench-utils';
 import {controllerFactory} from './controllerFactory';
 
-const {
-  attackValues,
-  descriptions,
-} = utils.getContent('nosqlInjection');
+const {attackValues, descriptions} = utils.getContent('nosqlInjection');
 
 export const nosqlInjectionControllers = controllerFactory('nosqlInjection', {
   locals: {

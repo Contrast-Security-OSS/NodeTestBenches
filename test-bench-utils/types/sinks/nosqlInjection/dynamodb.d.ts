5af9fe60a48b952643a5bdc30f3d4ee3f93f04e0
dmitriy petrov <dmitry.petrov@mentormate.com>
2022-11-29 20:23:06 +0200

    fix some stuff

--- test-bench-utils/types/sinks/nosqlInjection/dynamodb.d.ts
+++ test-bench-utils/types/sinks/nosqlInjection/dynamodb.d.ts
@@ -15,28 +15,29 @@ export function _aws_sdk_DynamoDB_prototype_executeStatement({ input }: {
 }, { safe, noop }?: {
     safe?: boolean;
     noop?: boolean;
-}): Promise<any>;
+}): Promise<"NOOP" | import("aws-sdk/lib/request").PromiseResult<AWS.DynamoDB.ExecuteStatementOutput, AWS.AWSError>>;
 export function _aws_sdk_client_dynamodb_ScanCommand_ComparisonOperator({ input }: {
     input: string;
 }, { safe, noop }?: {
     safe?: boolean;
     noop?: boolean;
-}): Promise<any>;
+}): Promise<"NOOP" | import("@aws-sdk/client-dynamodb").ScanCommandOutput>;
 export function _aws_sdk_client_dynamodb_ScanCommand_FilterExpression({ input }: {
     input: string;
 }, { safe, noop }?: {
     safe?: boolean;
     noop?: boolean;
-}): Promise<any>;
+}): Promise<"NOOP" | import("@aws-sdk/client-dynamodb").ScanCommandOutput>;
 export function _aws_sdk_client_dynamodb_ScanCommand_ProjectionExpression({ input }: {
     input: string;
 }, { safe, noop }?: {
     safe?: boolean;
     noop?: boolean;
-}): Promise<any>;
+}): Promise<"NOOP" | import("@aws-sdk/client-dynamodb").ScanCommandOutput>;
 export function _aws_sdk_client_dynamodb_ExecuteStatementCommand({ input }: {
     input: string;
 }, { safe, noop }?: {
     safe?: boolean;
     noop?: boolean;
-}): Promise<any>;
+}): Promise<"NOOP" | import("@aws-sdk/client-dynamodb").ExecuteStatementCommandOutput>;
+import AWS = require("aws-sdk");

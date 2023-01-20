bf6c603ad6a976cc1965d8463231eeb3508509e9
Dmitriy Petrov <dmitry.petrov@mentormate.com>
2023-01-20 19:15:20 +0200

    add a new endpoint related to mongo-nosql-injection (#341)
    * add a new endpoint
    
    * add a new endpoint
    
    * typos

--- test-bench-utils/types/sinks/nosqlInjection/index.d.ts
+++ test-bench-utils/types/sinks/nosqlInjection/index.d.ts
@@ -11,6 +11,18 @@ declare const _exports: {
         safe?: boolean;
         noop?: boolean;
     }) => Promise<string>;
+    'mongodb.Collection.prototype.findOne__$function': ({ input }: {
+        input: string;
+    }, { safe, noop }?: {
+        safe?: boolean;
+        noop?: boolean;
+    }) => Promise<string>;
+    'mongodb.Collection.prototype.findOneAndUpdate__$function': ({ input }: {
+        input: any;
+    }, { safe, noop }?: {
+        safe?: boolean;
+        noop?: boolean;
+    }) => Promise<string>;
     'mongodb.Collection.prototype.updateOne': ({ input }: {
         input: string;
     }, { safe, noop }?: {
@@ -34,7 +46,7 @@ declare const _exports: {
     }, { safe, noop }?: {
         safe?: boolean;
         noop?: boolean;
-    }) => Promise<any>;
+    }) => Promise<"NOOP" | import("aws-sdk/lib/request").PromiseResult<AWS.DynamoDB.DocumentClient.ScanOutput, AWS.AWSError>>;
     'aws-sdk.DynamoDB.prototype.makeRequest': ({ input }: {
         input: string;
     }, { safe, noop }?: {
@@ -46,31 +58,31 @@ declare const _exports: {
     }, { safe, noop }?: {
         safe?: boolean;
         noop?: boolean;
-    }) => Promise<any>;
+    }) => Promise<"NOOP" | import("aws-sdk/lib/request").PromiseResult<AWS.DynamoDB.ExecuteStatementOutput, AWS.AWSError>>;
     'aws-sdk.client-dynamodb.ScanCommand.ComparisonOperator': ({ input }: {
         input: string;
     }, { safe, noop }?: {
         safe?: boolean;
         noop?: boolean;
-    }) => Promise<any>;
+    }) => Promise<"NOOP" | import("@aws-sdk/client-dynamodb").ScanCommandOutput>;
     'aws-sdk.client-dynamodb.ScanCommand.FilterExpression': ({ input }: {
         input: string;
     }, { safe, noop }?: {
         safe?: boolean;
         noop?: boolean;
-    }) => Promise<any>;
+    }) => Promise<"NOOP" | import("@aws-sdk/client-dynamodb").ScanCommandOutput>;
     'aws-sdk.client-dynamodb.ScanCommand.ProjectionExpression': ({ input }: {
         input: string;
     }, { safe, noop }?: {
         safe?: boolean;
         noop?: boolean;
-    }) => Promise<any>;
+    }) => Promise<"NOOP" | import("@aws-sdk/client-dynamodb").ScanCommandOutput>;
     'aws-sdk.client-dynamodb.ExecuteStatementCommand': ({ input }: {
         input: string;
     }, { safe, noop }?: {
         safe?: boolean;
         noop?: boolean;
-    }) => Promise<any>;
+    }) => Promise<"NOOP" | import("@aws-sdk/client-dynamodb").ExecuteStatementCommandOutput>;
     'r.insert': ({ input }: {
         input: string;
     }, { safe, noop }?: {

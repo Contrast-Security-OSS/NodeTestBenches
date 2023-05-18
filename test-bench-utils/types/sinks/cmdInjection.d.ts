808b61a2760e0658f5bf9828e331ee739250eec4
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2023-05-18 16:06:44 +0300

    Add new cmdi routes

--- test-bench-utils/types/sinks/cmdInjection.d.ts
+++ test-bench-utils/types/sinks/cmdInjection.d.ts
@@ -16,7 +16,31 @@ export function _child_process_spawn({ input }: {
     safe?: boolean;
     noop?: boolean;
 }): Promise<any>;
+export function _child_process_spawn___second_argument({ input }: {
+    input: string;
+}, { safe, noop }?: {
+    safe?: boolean;
+    noop?: boolean;
+}): Promise<any>;
 export function _child_process_spawnSync({ input }: {
+    input: string;
+}, { safe, noop }?: {
+    safe?: boolean;
+    noop?: boolean;
+}): Promise<string>;
+export function _child_process_spawnSync___second_argument({ input }: {
+    input: string;
+}, { safe, noop }?: {
+    safe?: boolean;
+    noop?: boolean;
+}): Promise<string>;
+export function _child_process_execFile({ input }: {
+    input: string;
+}, { safe, noop }?: {
+    safe?: boolean;
+    noop?: boolean;
+}): Promise<any>;
+export function _child_process_execFileSync({ input }: {
     input: string;
 }, { safe, noop }?: {
     safe?: boolean;
